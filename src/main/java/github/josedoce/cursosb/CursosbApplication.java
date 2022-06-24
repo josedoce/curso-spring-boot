d469fc60cf8fc4ea444e88d56e98cec75037a642
JAS <66434808+josedoce@users.noreply.github.com>
2022-06-24 20:30:38 -0300

    Criando operação de instanciacao

--- src/main/java/github/josedoce/cursosb/CursosbApplication.java
+++ src/main/java/github/josedoce/cursosb/CursosbApplication.java
@@ -1,13 +1,31 @@
 package github.josedoce.cursosb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursosbApplication {
+import github.josedoce.cursosb.domain.Categoria;
+import github.josedoce.cursosb.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursosbApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		var cat1 = new Categoria(null, "Informática");
+		var cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 }
