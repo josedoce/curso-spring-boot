23a593403a56cd0a13bc238a3559429c343031fd
JAS <66434808+josedoce@users.noreply.github.com>
2022-06-24 17:37:05 -0300

    testando a primeira classe de dominio - Categoria

---
+++ src/main/java/github/josedoce/cursosb/domain/Categoria.java
@@ -0,0 +1,53 @@
+package github.josedoce.cursosb.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {}
+	
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
--- src/main/java/github/josedoce/cursosb/resources/CategoriaResource.java
+++ src/main/java/github/josedoce/cursosb/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package github.josedoce.cursosb.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import github.josedoce.cursosb.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@GetMapping("/")
-	public String listar() {
-		return "REST está funcionando.";
+	public List<Categoria> listar() {
+		var cat1 = new Categoria(1, "Informática");
+		var cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategorias = new ArrayList<>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		return listaCategorias;
 	}
 }
