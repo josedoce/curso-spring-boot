0ffa3c1b2d9cd7418a0eb4dc9bce445a338869ec
JAS <66434808+josedoce@users.noreply.github.com>
2022-06-24 17:15:02 -0300

    foi criado

---
+++ src/main/java/github/josedoce/cursosb/CursosbApplication.java
@@ -0,0 +1,13 @@
+package github.josedoce.cursosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/josedoce/cursosb/CursosbApplicationTests.java
@@ -0,0 +1,13 @@
+package github.josedoce.cursosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
