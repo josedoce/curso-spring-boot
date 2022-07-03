c7adf0a86e45572cc384afe68363478631fdfe87
JAS <66434808+josedoce@users.noreply.github.com>
2022-07-03 12:27:45 -0300

    Refresh token

---
+++ src/main/java/github/josedoce/cursosb/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package github.josedoce.cursosb.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import github.josedoce.cursosb.security.JWTUtil;
+import github.josedoce.cursosb.security.UserSpringSecurity;
+import github.josedoce.cursosb.services.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@PostMapping("/refresh_token")
+	public ResponseEntity<Void> regreshToken(HttpServletResponse response){
+		UserSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer "+token);
+		return ResponseEntity.noContent().build();
+	}
+}
